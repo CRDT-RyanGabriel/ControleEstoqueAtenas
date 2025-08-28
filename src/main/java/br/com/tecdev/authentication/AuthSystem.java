f25b827e3bfe76cb3409eeb6fc4400dafe8cf57b
GabrielSantiago-estudo <gabriel.santiago@uniatenas.edu.br>
2025-08-28 20:57:55 -0300

    AddClasses

---
+++ src/main/java/br/com/tecdev/authentication/AuthSystem.java
@@ -0,0 +1,5 @@
+public class AuthSystem {
+    public static void Login(String name, String password, String email){
+        
+    }
+}
---
+++ src/main/java/br/com/tecdev/authentication/User.java
@@ -0,0 +1,6 @@
+public class User{
+    private String name;
+    private String email;
+    private String password;
+    private String userId;  
+}
