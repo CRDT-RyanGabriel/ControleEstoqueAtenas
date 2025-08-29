5a88a0dd7cc95f0b51f144171e6c86d9f14d6ce7
GabrielSantiago-estudo <gabriel.santiago@uniatenas.edu.br>
2025-08-28 21:30:07 -0300

    chore: add class Permissions

---
+++ src/main/java/br/com/tecdev/authentication/Permissions.java
@@ -0,0 +1,7 @@
+package br.com.tecdev.authentication;
+public class Permissions {
+  
+    public static final String ADMIN = "ADMIN";
+    public static final String MANAGER = "MANAGER";
+    public static final String SELLER = "SELLER";
+}
